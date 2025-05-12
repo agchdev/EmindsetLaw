3e8ab6b919b5b6b489fd56518c5477d8f7c50424
agchdev <alex.chinchi02@gmail.com>
2025-05-12 17:58:55 +0200

    cambios

---
+++ EmindsetLaw/src/components/layout/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <a href="">Quienes somos</a>
+        <a href="">Soluciones</a>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        <a href="">Noticias</a>
+        <a href="">Contacto</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
