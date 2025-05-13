2c19344de2732bf7173a86fa9b55da1a5a228f2a
agchdev <alex.chinchi02@gmail.com>
2025-05-13 20:05:25 +0200

    cambios

--- EmindsetLaw/src/components/layout/Header.jsx
+++ EmindsetLaw/src/components/layout/Header.jsx
@@ -1,20 +1,73 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
+import logo from '../../assets/logo.png';
 
 const Header = () => {
+  
+  const menu = useRef(null)
+  const menuMovil = useRef(null)
+
+  const links = ['Quienes somos', 'Soluciones', 'Noticias', 'Contacto'];
+
+  useEffect(() => {
+    menu.current.classList.add('fade-in')
+    console.log(menuMovil.current)
+  }, [])
+
+  const toggleMenu = () => {
+    menuMovil.current.classList.toggle('hidden')
+  }
+
   return (
-    <header>
-      <div>
-        <a href="">Quienes somos</a>
-        <a href="">Soluciones</a>
-      </div>
-      <div>
-        
-      </div>
-      <div>
-        <a href="">Noticias</a>
-        <a href="">Contacto</a>
-      </div>
-    </header>
+    <>
+
+      <header>
+        {/* MOVIL */}
+        <div className="flex justify-between items-center">
+          <div className="hidden">
+            <a href="">Quienes somos</a>
+            <a href="">Soluciones</a>
+          </div>
+          <div
+            ref={menu}
+            className="w-[20%] m-auto relative"
+            onClick={() => {
+              toggleMenu()
+              menuMovil.current.classList.add('menu')
+            }}
+          >
+            <img className='absolute' onClick={(e) => e.target.classList.toggle('click-logo')} src={logo} alt="" />
+            <img src={logo} alt="" />
+          </div>
+          <div className="hidden">
+            <a href="">Noticias</a>
+            <a href="">Contacto</a>
+          </div>
+        </div>
+        <div
+          ref={menuMovil}
+          className='hidden'
+        >
+          <div className='flex flex-col text-center bg-[#1579a2] text-zinc-100 gap-5 pt-5 uppercase text-xl'>
+            {
+              links.map((link, index) => (
+                <>
+                  <div 
+                    key={index}
+                    className={`link-animate delay-${index * 200}ms px-3 flex justify-between`}
+                    style={{ animationDelay: `${index * 200}ms` }}
+                  >
+                    <a href="">{link}</a>
+                    <svg width={24} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" focusable="false" fill='white' ><g color="black, rgb(253, 251, 245))" weight="light"><path d="M220.24,132.24l-72,72a6,6,0,0,1-8.48-8.48L201.51,134H40a6,6,0,0,1,0-12H201.51L139.76,60.24a6,6,0,0,1,8.48-8.48l72,72A6,6,0,0,1,220.24,132.24Z"></path></g></svg>
+                  </div>
+                  <div className='h-[.2px] bg-white/50 w-[100%]'></div>
+                </>
+              ))
+            }
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
 
