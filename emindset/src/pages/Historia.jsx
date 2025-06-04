fe20b978898ea6851c376d314ec453f4486c1ac1
agchdev <alex.chinchi02@gmail.com>
2025-06-04 10:34:52 +0200

    Add: Traducciones completas para la sección de contacto en todos los idiomas

--- emindset/src/pages/Historia.jsx
+++ emindset/src/pages/Historia.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import { motion } from 'framer-motion';
+
 
 const Historia = () => {
   // Inicializar el hook de traducción
